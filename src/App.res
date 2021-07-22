1502bde02a5463b1102a2e4fd8686552b1e7cab4
ascandone <alescandone@gmail.com>
2021-07-22 15:26:46 +0200

    scaffolded vite app

---
+++ src/App.res
@@ -0,0 +1,2 @@
+@react.component
+let default = () => React.string("Hello")
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'todomvc-common/base.css'
+import 'todomvc-app-css/index.css'
+import App from './App.bs'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />    
+  </React.StrictMode>,
+  document.getElementById('root')
+)
